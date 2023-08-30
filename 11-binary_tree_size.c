74b2550bbabc85f46637146763d37f9fcacf416f
Kemelite1 <osikemelite42@gmail.com>
2023-08-30 08:51:07 -0700

    size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,6 +12,5 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	size_t left_size = binary_tree(tree->left);
 	size_t right_size = binary_tree(tree->right);
 
-	return left_size + right_size + 1;
-/** total size of the tree is the sum of the sizes of the left and right subtrees, plus 1 for the current node*/
+	return size;
 }
